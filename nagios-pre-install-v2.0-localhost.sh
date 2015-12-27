becdaedbf486d08ad1f1c73966a96ac03debad05
houqian <houqian1991@foxmail.com>
2015-12-27 23:34:21 +0800

    fix sed error

--- nagios-pre-install-v2.0-localhost.sh
+++ nagios-pre-install-v2.0-localhost.sh
@@ -52,7 +52,7 @@ if [ $OS = "RedHat" ]; then
     echo -e "\n"
 fi
 
-echo -e "${green}Yum install OneAlert Agent Succeful!${endColor}"
+echo -e "${green}Yum install OneAlert Agent Successful!${endColor}"
 echo -e "\n"
 
 echo -e "Start to set configuration..."
@@ -62,7 +62,8 @@ $sudo_cmd chmod -R +x /usr/local/nagios/libexec/nagios
 if [ -e /usr/local/nagios/etc/objects/110monitor.cfg ]; then
     echo -e "\033[34m\n* Adding your license key to the Agent configuration: /usr/local/nagios/etc/objects/110monitor.cfg\n\033[0m\n"
     $sudo_cmd cp /usr/local/nagios/etc/objects/110monitor.cfg /usr/local/nagios/etc/objects/110monitor.cfg.example
-    $sudo_cmd sh -c "sed -i 's/your-app-key:.*/your-app-key: $NAGIOS_APPKEY/' /usr/local/nagios/etc/objects/110monitor.cfg.example > /usr/local/nagios/etc/objects/110monitor.cfg"
+    $sudo_cmd sed -i "s/your_app_key/$NAGIOS_APPKEY/g" /usr/local/nagios/etc/objects/110monitor.cfg.example
+    $sudo_cmd sed -i "s/your_app_key/$NAGIOS_APPKEY/g" /usr/local/nagios/etc/objects/110monitor.cfg
     $sudo_cmd sed -i "s/your_app_key/$NAGIOS_APPKEY/g" /usr/local/nagios/libexec/alert-agent/conf/runtime.properties
     $sudo_cmd sed -i "s/alert_url/$MENU_CEP_URL/g" /usr/local/nagios/libexec/alert-agent/conf/runtime.properties
     $sudo_cmd sed -i "s/hb_url/$HB_URL/g" /usr/local/nagios/libexec/alert-agent/conf/runtime.properties
